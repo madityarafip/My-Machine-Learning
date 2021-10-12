c6f3292b9d2424a459decb56ee3c5d47750cdbf8
Mohammad Aditya Rafi Pratama <42953630+madityarafip@users.noreply.github.com>
2021-10-12 14:37:07 +0700

    Add files via upload

---
+++ ML-Zoomcamp/HW-Week-5/HW5-Codes/test_hw5.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://localhost:1208/predict_with_flask_hw5"
+
+customer_id = 'aed-313'
+customer = {"contract": "two_year", "tenure": 1, "monthlycharges": 10}
+
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+if response["Churn"] == True:
+    print('Sending promo email to %s' % customer_id)
+else:
+    print('Not sending promo email to %s' % customer_id)
